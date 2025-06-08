41ac873e682133e7bbf3da6e422fd9c2f563318a
Yubeen Lee <lybin71@naver.com>
2025-06-08 14:27:13 +0100

    feat: add about-us, not-found page, and a nav bar

---
+++ components/navigation.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function Navigation() {
+  const path = usePathname();
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          {path === "/" ? "🔥" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About us</Link>
+          {path === "/about-us" ? "🔥" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
